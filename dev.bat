060ed2b213d0477369134e11bca7cdbb9fab9048
babis.marmanis <babis.marmanis@9676a798-0aaa-11de-85a1-fd18384f7567>
2009-04-27 22:46:47 +0000

    Added two more shell environments, one for running the server and one for using the sample service.

--- dev.bat
+++ dev.bat
@@ -1,3 +1,8 @@
 start "PlinthOS  [build]" /DC:\code\plinthos\plinthos\build
 
 start "POS Statistics Service  [build]" /DC:\code\plinthos\plinthos-samples\StatisticsService\build
+
+start "PlinthOS  [Server]" /DC:\POSx\server
+
+start "POS Statistics Service  [Sample Client]" /DC:\POSx\samples\StatisticsService
+
