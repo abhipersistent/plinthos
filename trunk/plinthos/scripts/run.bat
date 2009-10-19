b8c9a11b2c715bd7c5ea1202cfc608cbb6204f09
dbabenko <dbabenko@9676a798-0aaa-11de-85a1-fd18384f7567>
2009-10-19 18:47:28 +0000

    Added connection pool for mysql configuration.

--- trunk/plinthos/scripts/run.bat
+++ trunk/plinthos/scripts/run.bat
@@ -14,6 +14,7 @@ set CLASSPATH=%PLINTHOS_DIR%\lib\commons-logging.jar;%CLASSPATH%
 set CLASSPATH=%PLINTHOS_DIR%\lib\dom4j-1.6.1.jar;%CLASSPATH%
 set CLASSPATH=%PLINTHOS_DIR%\lib\hibernate-annotations.jar;%CLASSPATH%
 set CLASSPATH=%PLINTHOS_DIR%\lib\hibernate3.jar;%CLASSPATH%
+set CLASSPATH=%PLINTHOS_DIR%\lib\c3p0-0.9.1.jar;%CLASSPATH%
 set CLASSPATH=%PLINTHOS_DIR%\lib\hsqldb.jar;%CLASSPATH%
 set CLASSPATH=%PLINTHOS_DIR%\lib\javassist-3.4.GA.jar;%CLASSPATH%
 set CLASSPATH=%PLINTHOS_DIR%\lib\jetty-6.1.15.jar;%CLASSPATH%
@@ -28,4 +29,7 @@ set CLASSPATH=%PLINTHOS_DIR%\lib\xpp3_min-1.1.4c.jar;%CLASSPATH%
 set CLASSPATH=%PLINTHOS_DIR%\lib\xstream-1.3.1.jar;%CLASSPATH%
 set CLASSPATH=%PLINTHOS_DIR%\lib\commons-io-1.4.jar;%CLASSPATH%
 
-java -cp %CLASSPATH% -Dplinthos.dir=%PLINTHOS_DIR% -Dlog4j.configuration=%PLINTHOS_NODE_DIR%/log4j.xml -Dnode.properties=%PLINTHOS_NODE_DIR%/node.properties org.plinthos.application.PlinthOSApp
+set JPDA=
+@rem set JPDA=-Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,address=8788,server=y,suspend=n
+
+java -cp %CLASSPATH% %JPDA% -Dplinthos.dir=%PLINTHOS_DIR% -Dlog4j.configuration=%PLINTHOS_NODE_DIR%/log4j.xml -Dnode.properties=%PLINTHOS_NODE_DIR%/node.properties org.plinthos.application.PlinthOSApp
