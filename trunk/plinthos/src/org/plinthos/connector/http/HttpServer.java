b0f86898d3132636a6307edf87ed3a964fc23c3f
babis.marmanis@gmail.com <babis.marmanis@gmail.com@9676a798-0aaa-11de-85a1-fd18384f7567>
2013-10-04 23:32:41 +0000

    Upgrading Jetty, xstream, hsqlDB, commons.lang, and slfj

--- trunk/plinthos/src/org/plinthos/connector/http/HttpServer.java
+++ trunk/plinthos/src/org/plinthos/connector/http/HttpServer.java
@@ -30,75 +30,203 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.log4j.Logger;
-import org.mortbay.jetty.Connector;
-import org.mortbay.jetty.Server;
-import org.mortbay.jetty.bio.SocketConnector;
-import org.mortbay.jetty.servlet.ServletHandler;
-import org.mortbay.jetty.servlet.ServletHolder;
+import org.eclipse.jetty.server.Connector;
+import org.eclipse.jetty.server.HttpConfiguration;
+import org.eclipse.jetty.server.HttpConnectionFactory;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.ServerConnector;
+import org.eclipse.jetty.servlet.ServletHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
 
 public class HttpServer {
 
 	@SuppressWarnings("unused")
 	private static final Logger logger = Logger.getLogger(HttpServer.class);
+
+	public static final int DEFAULT_IDLE_TIMEOUT=30000;
+	public static final int DEFAULT_OUTPUT_BUFFER_SIZE=32768;
+	public static final int DEFAULT_PORT = 8080;
+	public static final int DEFAULT_SECURE_PORT = 8443;
+	public static final String DEFAULT_SECURE_SCHEME="https";
 	
-	private static final int DEFAULT_PORT = 8080;
-	
-	private int port = DEFAULT_PORT;
+	private int idleTimeout      = DEFAULT_IDLE_TIMEOUT;
+	private int outputBufferSize = DEFAULT_OUTPUT_BUFFER_SIZE;
+	private int port             = DEFAULT_PORT;
+	private int securePort       = DEFAULT_SECURE_PORT;
+
+	private String startedOn;
 	
 	public HttpServer() {
-	
+		startedOn= java.util.Calendar.getInstance().getTime().toString();
 	}
 
 	public void setPort(int port) {
 		this.port = port;
 	}
-	
-	public void start() {
-		//TODO: HttpServer vs Server
-	    Server server = new Server();
-	    Connector connector=new SocketConnector();
-	    connector.setPort(port);
-	    server.setConnectors(new Connector[]{connector});
-	    
-	    ServletHandler handler=new ServletHandler();
-	    server.setHandler(handler);
 
-	    
-	    handler.addServletWithMapping("org.plinthos.connector.http.HttpServer$RootServlet", "/");	    
-	    
-	    handler.addServletWithMapping("org.plinthos.connector.http.UrlGatewayServlet", "/plinthos/test");
-	    handler.addServletWithMapping(new ServletHolder(new XmlRpcGatewayServlet()), "/plinthos/gateway");
-	    
-	    try {
-	    	server.start();
-	    }
-	    catch(Exception e) {
-	    	throw new RuntimeException(e);
-	    }
-	}
-	
-	public void shutdown() {
+	public void start() {
+		// Create a basic Jetty server object that will be configured 
+		// for multiple	connectors. This is code straight from the Jetty example
+		Server server = new Server();
+
+		// HTTP Configuration
+		// HttpConfiguration is a collection of configuration information appropriate 
+		// for http and https. The default scheme for http is <code>http</code> of course, 
+		// as the default for secured http is <code>https</code>. 
+		HttpConfiguration http_config = new HttpConfiguration();
+		http_config.setSecureScheme(DEFAULT_SECURE_SCHEME);
+		http_config.setSecurePort(securePort);
+		http_config.setOutputBufferSize(outputBufferSize);
+
+		// HTTP connector
+		// The first server connector we create is the one for http, passing in
+		// the http configuration we configured
+		// above so it can get things like the output buffer size, etc. We also
+		// set the port (8080) and configure an
+		// idle timeout.
+		ServerConnector http = new ServerConnector(server,
+				new HttpConnectionFactory(http_config));
+		http.setPort(port);
+		http.setIdleTimeout(idleTimeout);
+
+		// SSL Context Factory for HTTPS and SPDY
+		// SSL requires a certificate so we configure a factory for ssl contents
+		// with information pointing to what
+		// keystore the ssl connection needs to know about. Much more
+		// configuration is available the ssl context,
+		// including things like choosing the particular certificate out of a
+		// keystore to be used.
+		/*
+			SslContextFactory sslContextFactory = new SslContextFactory();
+			sslContextFactory.setKeyStorePath(jetty_home + "/etc/keystore");
+			sslContextFactory
+					.setKeyStorePassword("OBF:1vny1zlo1x8e1vnw1vn61x8g1zlu1vn4");
+			sslContextFactory.setKeyManagerPassword("OBF:1u2u1wml1z7s1z7a1wnl1u2g");
+		 */
+
+		// HTTPS Configuration
+		// A new HttpConfiguration object is needed for the next connector and
+		// you can pass the old one as an
+		// argument to effectively clone the contents. On this HttpConfiguration
+		// object we add a
+		// SecureRequestCustomizer which is how a new connector is able to
+		// resolve the https connection before
+		// handing control over to the Jetty Server.
+		/*
+			HttpConfiguration https_config = new HttpConfiguration(http_config);
+			https_config.addCustomizer(new SecureRequestCustomizer());
+		 */
+
+		// HTTPS connector
+		// We create a second ServerConnector, passing in the http configuration
+		// we just made along with the
+		// previously created ssl context factory. Next we set the port and a
+		// longer idle timeout.
+		/*
+			ServerConnector https = new ServerConnector(server,
+					new SslConnectionFactory(sslContextFactory, "http/1.1"),
+					new HttpConnectionFactory(https_config));
+			https.setPort(8443);
+			https.setIdleTimeout(500000);
+		 */
+
+		// Here you see the server having multiple connectors registered with
+		// it, now requests can flow into the server
+		// from both http and https urls to their respective ports and be
+		// processed accordingly by jetty. A simple
+		// handler is also registered with the server so the example has
+		// something to pass requests off to.
+
+		// Set the connectors
+		server.setConnectors(new Connector[] { http});
+
+		ServletHandler handler = new ServletHandler();
+		server.setHandler(handler);
+
+		handler.addServletWithMapping("org.plinthos.connector.http.HttpServer$RootServlet", "/");
+
+		handler.addServletWithMapping("org.plinthos.connector.http.UrlGatewayServlet",
+				"/plinthos/test");
 		
+		handler.addServletWithMapping(new ServletHolder(new XmlRpcGatewayServlet()), "/plinthos/gateway");
+
+		try {
+			server.start();
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
 	}
-	
-	public static class RootServlet extends HttpServlet
-	{
+
+	public void shutdown() {
+
+	}
+
+	public static class RootServlet extends HttpServlet {
 		private static final long serialVersionUID = -1397617509572585771L;
 
-		protected void doGet(HttpServletRequest request, HttpServletResponse response) 
-	    	throws ServletException, IOException
-	    {
-	        response.setContentType("text/html");
-	        response.setStatus(HttpServletResponse.SC_OK);
+		protected void doGet(HttpServletRequest request,
+				HttpServletResponse response) throws ServletException,
+				IOException {
+			response.setContentType("text/html");
+			response.setStatus(HttpServletResponse.SC_OK);
 
-	        response.addHeader("Pragma", "no-cache");
-	        response.addHeader("Expires", "-1");
-	        response.addHeader("Cache-Control", "no-cache");
-	        
-	        Date date = new Date();
-	        response.getWriter().println(
-	        		"<html><body>PlinthOS, " + date.toString() + "</body></html>");
-	    }
+			response.addHeader("Pragma", "no-cache");
+			response.addHeader("Expires", "-1");
+			response.addHeader("Cache-Control", "no-cache");
+
+			Date date = new Date();
+			response.getWriter().println("<html><body><h1>Welcome to <b>PlinthOS</b></h1>"+
+			"<br/><p> The date/time is " + date.toString() + "</p></body></html>");
+		}
 	}
-	
+
+	/**
+	 * @return the idleTimeout
+	 */
+	public int getIdleTimeout() {
+		return idleTimeout;
+	}
+
+	/**
+	 * @param idleTimeout the idleTimeout to set
+	 */
+	public void setIdleTimeout(int idleTimeout) {
+		this.idleTimeout = idleTimeout;
+	}
+
+	/**
+	 * @return the outputBufferSize
+	 */
+	public int getOutputBufferSize() {
+		return outputBufferSize;
+	}
+
+	/**
+	 * @param outputBufferSize the outputBufferSize to set
+	 */
+	public void setOutputBufferSize(int outputBufferSize) {
+		this.outputBufferSize = outputBufferSize;
+	}
+
+	/**
+	 * @return the port
+	 */
+	public int getPort() {
+		return port;
+	}
+
+	/**
+	 * @return the securePort
+	 */
+	public int getSecurePort() {
+		return securePort;
+	}
+
+	/**
+	 * @return the startedOn
+	 */
+	public String getStartedOn() {
+		return startedOn;
+	}
+
 }
