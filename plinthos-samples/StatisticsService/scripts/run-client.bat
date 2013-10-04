35f56f00c75d312cf8ebc5438114213785632aa0
babis.marmanis <babis.marmanis@gmail.com>
2013-10-04 23:35:12 +0000

    Upgrading Jetty, xstream, hsqlDB, commons.lang, and slfj

--- plinthos-samples/StatisticsService/scripts/run-client.bat
+++ plinthos-samples/StatisticsService/scripts/run-client.bat
@@ -9,6 +9,6 @@ set CLASSPATH=%CLIENT_HOME_DIR%\lib\mysql-connector-java-5.1.7-bin.jar;%CLASSPAT
 set CLASSPATH=%CLIENT_HOME_DIR%\lib\plinthos-shared.jar;%CLASSPATH%
 set CLASSPATH=%CLIENT_HOME_DIR%\lib\xercesImpl.jar;%CLASSPATH%
 set CLASSPATH=%CLIENT_HOME_DIR%\lib\xpp3_min-1.1.4c.jar;%CLASSPATH%
-set CLASSPATH=%CLIENT_HOME_DIR%\lib\xstream-1.3.1.jar;%CLASSPATH%
+set CLASSPATH=%CLIENT_HOME_DIR%\lib\xstream-1.4.5.jar;%CLASSPATH%
 
 java -cp %CLASSPATH% org.plinthos.sample.statistics.client.BasicStatisticsClient %1 %2
